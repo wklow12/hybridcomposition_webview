3fb2b4a9d34aa103df9ca1350f3001190e0a56b8
waikit.low <waikit.low@redone.com.my>
2021-11-11 21:31:00 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/hybridcomposition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hybridcomposition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          child: Text('click'),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return MyWebViewPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class MyWebViewPage extends StatefulWidget {
+  @override
+  _MyWebViewPageState createState() => _MyWebViewPageState();
+}
+
+class _MyWebViewPageState extends State<MyWebViewPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://www.google.com',
+    );
+  }
+}
